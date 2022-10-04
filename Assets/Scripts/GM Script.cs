68e1eef62f56456dcb76284dd47974504f6100b3
Alessandro Picchi <alessandro.picchi95@gmail.com>
2022-10-04 02:16:03 +0200

    Lightmaps & smoothgrab

---
+++ Assets/Scripts/GM Script.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMScript : MonoBehaviour
+{
+    void Start()
+    {
+        OVRPlugin.systemDisplayFrequency = 72.0f;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ItemInteractorLHand.cs
+++ Assets/Scripts/ItemInteractorLHand.cs
@@ -23,12 +23,13 @@ public class ItemInteractorLHand : MonoBehaviour
         if (other.gameObject.tag == "Grabbable" && OVRInput.Get(OVRInput.Button.PrimaryHandTrigger) && OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
         {
             inHand = other.gameObject;
-            Vector3 objPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-            other.transform.position = objPos;
-            other.transform.rotation = this.transform.rotation;
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            other.gameObject.transform.SetParent(this.transform);
         }
         else if (inHand != null)
         {
+            other.gameObject.transform.SetParent(null);
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
             var velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
             inHand.GetComponent<Rigidbody>().velocity = velocity;
             inHand = null;
